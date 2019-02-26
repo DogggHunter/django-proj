341af327836574e11792babff2635185ed70dae8
DogHunter <trainer875@gmail.com>
2019-02-26 18:33:44 +0200

    Clean and update forms.py

--- testing/testingapp/forms.py
+++ testing/testingapp/forms.py
@@ -7,16 +7,6 @@ class TestRunAnswersForm(ModelForm):
     class Meta:
         model = TestRunAnswers
         fields = ('answer', )
-        # widgets = {
-        #     'answer': TextInput(attrs={'required': 'true'})
-        # }
-
-data = {
-    'form-TOTAL_FORMS': '3',
-    'form-INITIAL_FORMS': '0',
-    'form-MIN_NUM_FORMS': '2',
-    'form-MAX_NUM_FORMS': '1000',
-    'form-0-answer': 'fds',
-    'form-1-answer': 'fdg',
-    'form-2-answer': 'fdg',
-}
+        widgets = {
+            'answer': TextInput(attrs={'required': 'true'})
+        }
